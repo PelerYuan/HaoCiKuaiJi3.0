b71f1190c092d5ff621f5b49a9bb472b377e8a50
PelerYuan <pelerlittlepig@gmail.com>
2024-09-26 20:52:55 +0800

    add test

--- DataStract/WordGroup.py
+++ DataStract/WordGroup.py
@@ -1,3 +1,6 @@
+from Functions.CSV import open_csv
+
+
 class WordGroup:
     def __init__(self, name:str):
-        self.word_data = data_list
+        self.word_data = open_csv(f'data/words/{name}.csv')
