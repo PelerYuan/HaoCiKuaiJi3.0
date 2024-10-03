26e720d8230d69f466479859dec5ba62595cb7ab
PelerYuan <pelerlittlepig@gmail.com>
2024-10-03 22:52:06 +0800

    review part

--- Block/MemorizeBlock.py
+++ Block/MemorizeBlock.py
@@ -5,6 +5,7 @@ from PySide6.QtCore import QModelIndex
 from PySide6.QtWidgets import QTableWidgetItem, QTableWidget
 
 from DataStract.WordMemorize import WordMemorize
+from Functions.JSON import open_json
 from Functions.WordMemorize import init_memorize, open_memorize
 from Models.MemorizeWordModel import MemorizeWordModel
 from Models.TestResultModel import TestResultModel
@@ -19,21 +20,35 @@ class MemorizeBlock:
         self.ui = ui
 
     def event_connect(self):
-        self.ui.memorize_new_listWidget.doubleClicked.connect(self.start_memorize)
+        self.ui.memorize_new_listWidget.itemDoubleClicked.connect(self.start_memorize)
+        self.ui.memorize_review_listWidget.itemDoubleClicked.connect(self.start_memorize)
 
     def init(self):
+        self.ui.memorize_new_listWidget.clear()
+        self.ui.memorize_review_listWidget.clear()
+
         self.ui.memorize_new_listWidget.addItems(get_all_group())
         init_memorize()
+        index = open_json('./data/memorize/index.json')
+        index = dict(sorted(index.items(), key=lambda item: item[1]['last_time']))
+        count = 0
+        for group in index.keys():
+            if count <= 5:
+                if index[group]['last_time']:
+                    self.ui.memorize_review_listWidget.addItem(group)
+                    count += 1
+            else:
+                break
 
-    def start_memorize(self, item:QModelIndex):
-        group_name = self.ui.memorize_new_listWidget.item(item.row()).text()
+    def start_memorize(self, item):
+        group_name = item.text()
         word_memorize = MemorizeWordModel(WordGroup(group_name))
         result = word_memorize.exec_()
         if result:
             memorize = open_memorize(group_name)
-            print(result)
             memorize.set_data(result)
             memorize.save_data()
+            self.init()
 
 
     def close(self):
